b053fd5c3361e9f333c60551c9b1d6a6965f631b
Skoomaki <skoomaki12@gmail.com>
2022-07-02 15:49:05 +0100

    inital commit of player logic and simple flooring

---
+++ Kelsier.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+var player_velocity = Vector2(0,-1)
+
+const SPEED = 230
+const NORMAL_JUMP_HEIGHT = -1000
+const GRAVITY = 37
+
+func _physics_process(delta):
+	if Input.is_action_pressed("player_right"): 
+		player_velocity.x = SPEED
+	if Input.is_action_pressed("player_left"): 
+		player_velocity.x = -SPEED
+	if Input.is_action_just_pressed("player_jump") and is_on_floor():
+		player_velocity.y = NORMAL_JUMP_HEIGHT
+	
+	player_velocity.y = player_velocity.y + GRAVITY
+		
+	player_velocity = move_and_slide(player_velocity, Vector2.UP)
+
+	player_velocity.x = lerp(player_velocity.x,0,0.11)
