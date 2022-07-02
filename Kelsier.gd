f2a452598d4e7aba185bbe9508aa867f6e9f3e01
Skoomaki <skoomaki12@gmail.com>
2022-07-02 22:18:27 +0100

    double jump added

--- Kelsier.gd
+++ Kelsier.gd
@@ -3,8 +3,9 @@ extends KinematicBody2D
 var player_velocity = Vector2(0,-1)
 
 const SPEED = 400
-const NORMAL_JUMP_HEIGHT = -1000
+const NORMAL_JUMP_HEIGHT = -700
 const GRAVITY = 37
+var COUNT_DOUBLE_JUMP = 0
 
 func _physics_process(delta):
 	if Input.is_action_pressed("player_right"): 
@@ -17,14 +18,15 @@ func _physics_process(delta):
 		$Sprite.flip_h = true
 	else:
 		$Sprite.play("idle")
-		
 	if not is_on_floor():
 			$Sprite.play("air")
-	if Input.is_action_just_pressed("player_jump") and is_on_floor():
+	if Input.is_action_just_pressed("player_jump") && COUNT_DOUBLE_JUMP < 1:
 		player_velocity.y = NORMAL_JUMP_HEIGHT
+		COUNT_DOUBLE_JUMP += 1
+		
+	if is_on_floor():
+		COUNT_DOUBLE_JUMP = 0
 	
 	player_velocity.y = player_velocity.y + GRAVITY
-		
 	player_velocity = move_and_slide(player_velocity, Vector2.UP)
-
 	player_velocity.x = lerp(player_velocity.x,0,0.89)
